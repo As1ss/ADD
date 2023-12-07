e1b0914a1d5d3019461f51ccbc880ce86a178fb4
As1ss <alexis.lopbri@educa.jcyl.es>
2023-12-07 21:24:09 +0100

    Ejemplo de transfomracion visual de un documento XML a partir de un documento XSL

--- JAVA/RepasoSAX/src/Ejemplos/TransformarXMLconXSL.java
+++ JAVA/RepasoSAX/src/Ejemplos/TransformarXMLconXSL.java
@@ -1,9 +1,50 @@
 package Ejemplos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
 public class TransformarXMLconXSL {
 
 	public static void main(String[] args) {
-		String sourcePath = "D://Personas.xml";
+		
+		String dataPath = "C:\\Users\\As1ss\\Downloads\\alumnos.xml";
+		String stylePath = "C:\\Users\\As1ss\\Downloads\\alumnosPlantilla.xsl"; //La estructura del xsl tiene que coincidir con la del xml de la data
+		File ficheroHTML = new File("D://Mipagina.html");
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(ficheroHTML);
+			Source sourceData = new StreamSource(dataPath);
+			Source sourceStyle = new StreamSource(stylePath);
+			Result result = new StreamResult(fos);
+			
+			Transformer transformer = TransformerFactory.newInstance().newTransformer(sourceStyle);
+			transformer.transform(sourceData, result);
+			
+			fos.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		
 
 	}
 
