caf6c37bfcd842342c4dc30954e084bbada01677
As1ss <alexis.lopbri@educa.jcyl.es>
2023-12-20 17:53:17 +0100

    Practica nº realizado modificacion, eliminacion queda modifacion stock y fixear la devolucion booleana de metdoos

--- JAVA/EjemploXSDT/src/operaciones/principal.java
+++ JAVA/EjemploXSDT/src/operaciones/principal.java
@@ -4,11 +4,15 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.print.attribute.standard.DateTimeAtCompleted;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
@@ -29,6 +33,7 @@ public class principal {
 	}
 
 	private static void menuPrincipal() {
+
 		String opcion = "";
 		do {
 			System.out.println("-------MENU PRINCIPAL-------");
@@ -37,9 +42,10 @@ public class principal {
 			System.out.println("2) Insertar venta");
 			System.out.println("3) Modificar venta");
 			System.out.println("4) Eliminar venta");
-			System.out.println("5) Salir");
+			System.out.println("5) Añadir stock");
+			System.out.println("6) Salir");
+			System.out.println("------------------------------");
 			opcion = sc.nextLine();
-			
 
 			switch (opcion) {
 			case "1":
@@ -61,13 +67,211 @@ public class principal {
 					insertarventa(numVenta, nombreCli, unidadesVenta, fechaVenta);
 				} catch (InputMismatchException e) {
 					System.out.println("Has introducido un caracter no válido");
-					
+
 				}
-				
+
+				break;
+			case "3":
+				try {
+					System.out.println("Inserta el número de venta (Digito) quer quieras modificar");
+					int numVenta = sc.nextInt();
+					sc.nextLine();
+					if (modificarVenta(numVenta)) {
+						System.out.println("Se ha realizado la operación de modificación.");
+					} else {
+						System.err.println("Ha ocurrido un error, nos e ha realizado la modificación.");
+					}
+
+				} catch (InputMismatchException e) {
+					System.out.println("Has introducido un caracter no válido");
+
+				}
+
+				break;
+			case "4":
+				try {
+					System.out.println("Inserta el número de venta (Digito) que quieras eliminar");
+					int numVenta = sc.nextInt();
+					sc.nextLine();
+					if (eliminarVenta(numVenta)) {
+						System.out.println("Se ha realizado la operación de eliminacion.");
+					} else {
+						System.err.println("Ha ocurrido un error, nos e ha realizado la eliminacion.");
+					}
+
+				} catch (InputMismatchException e) {
+					System.out.println("Has introducido un caracter no válido");
+
+				}
+
+				break;
+			case "5":
+				System.out.println("Inserta el número de unidades de stock que quieres añadir al artículo");
+				try {
+					int cantidad = sc.nextInt();
+					sc.nextLine();
+					if (añadirStock(cantidad)) {
+						System.out.println("El stock se ha actualizado con éxito.");
+					} else {
+						System.err.println("El stock se ha actualizado con éxito.");
+					}
+
+				} catch (Exception e) {
+					System.out.println(
+							"Ha ocurrido un error, nos e ha añadido la cantidad deseada al stock del artículo.");
+				}
 				break;
 			}
 
-		} while (!opcion.equals("5"));
+		} while (!opcion.equals("6"));
+		System.out.println("Has escogido salir.\n" + LocalDateTime.now().getDayOfMonth() +"/"+LocalDateTime.now().getMonth()
+				+"/"+LocalDateTime.now().getYear());
+	}
+
+	private static boolean añadirStock(int cantidad) {
+		System.out.println("--------------------------------");
+		System.out.println("--------- AÑADIR STOCK ---------");
+		System.out.println("--------------------------------");
+
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(generated.ObjectFactory.class);
+			Unmarshaller u = jaxbContext.createUnmarshaller();
+			JAXBElement jaxbElement = (JAXBElement) u.unmarshal(new FileInputStream("./ventasarticulos.xml"));
+
+			VentasType miventa = (VentasType) jaxbElement.getValue();
+
+			// Crear una instancia para obtener todas las ventas
+			Ventas vent = miventa.getVentas();
+			DatosArtic articulo = miventa.getArticulo();
+			int stockActualizado = articulo.getStock().intValue() + cantidad;
+			articulo.setStock(BigInteger.valueOf(stockActualizado));
+
+			System.out.println("Stock actualizado.");
+
+			// Crear el Marshaller, y volcar la lista al fichero XML
+			Marshaller m = jaxbContext.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			m.marshal(jaxbElement, new FileOutputStream("./ventasarticulos.xml"));
+
+			return true;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+
+	}
+
+	private static boolean eliminarVenta(int numeventa) {
+		boolean elimOk = false;
+		System.out.println("--------------------------------");
+		System.out.println("--------- ELIMINAR VENTA ---------");
+		System.out.println("--------------------------------");
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(generated.ObjectFactory.class);
+			Unmarshaller u = jaxbContext.createUnmarshaller();
+			JAXBElement jaxbElement = (JAXBElement) u.unmarshal(new FileInputStream("./ventasarticulos.xml"));
+
+			VentasType miventa = (VentasType) jaxbElement.getValue();
+
+			// Crear una instancia para obtener todas las ventas
+			Ventas vent = miventa.getVentas();
+
+			// Guardar las ventas en la lista
+			List<Ventas.Venta> listaVentas = new ArrayList<>();
+			listaVentas = vent.getVenta();
+
+			// Comprobar si existe el número de venta recorriendo el ArrayList
+
+			Ventas.Venta ve = verificarVenta(numeventa, listaVentas);
+
+			// Si el número de venta no existe, se añade la venta
+			if (ve != null) {
+				// Crear el objeto Ventas.Venta, y añadirle los datos
+
+				listaVentas.remove(ve);
+
+				// Crear el Marshaller, y volcar la lista al fichero XML
+				Marshaller m = jaxbContext.createMarshaller();
+				m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+				m.marshal(jaxbElement, new FileOutputStream("./ventasarticulos.xml"));
+				System.out.println("Venta " + numeventa + " eliminado.");
+				elimOk = true;
+
+			} else {
+				System.out.println("El número de venta " + numeventa + " no existe");
+				elimOk = false;
+			}
+
+		} catch (JAXBException je) {
+			System.out.println(je.getCause());
+		} catch (IOException ioe) {
+			System.out.println(ioe.getMessage());
+		}
+
+		return elimOk;
+
+	}
+
+	private static boolean modificarVenta(int numeventa) {
+		boolean modifOk = false;
+		System.out.println("--------------------------------");
+		System.out.println("--------- MODIFICAR VENTA ---------");
+		System.out.println("--------------------------------");
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(generated.ObjectFactory.class);
+			Unmarshaller u = jaxbContext.createUnmarshaller();
+			JAXBElement jaxbElement = (JAXBElement) u.unmarshal(new FileInputStream("./ventasarticulos.xml"));
+
+			VentasType miventa = (VentasType) jaxbElement.getValue();
+
+			// Crear una instancia para obtener todas las ventas
+			Ventas vent = miventa.getVentas();
+
+			// Guardar las ventas en la lista
+			List<Ventas.Venta> listaVentas = new ArrayList<>();
+			listaVentas = vent.getVenta();
+
+			// Comprobar si existe el número de venta recorriendo el ArrayList
+
+			Ventas.Venta ve = verificarVenta(numeventa, listaVentas);
+
+			// Si el número de venta no existe, se añade la venta
+			if (ve != null) {
+				// Crear el objeto Ventas.Venta, y añadirle los datos
+
+				System.out.println("Inserta las nuevas unidades (Dígito)");
+				int unidadesVenta = sc.nextInt();
+				sc.nextLine();
+
+				System.out.println("Inserta la nueva fecha de venta");
+				String fechaVenta = sc.nextLine();
+				ve.setFecha(fechaVenta);
+				ve.setUnidades(unidadesVenta);
+
+				// Añadir la venta a la lista
+				listaVentas.add(ve);
+
+				// Crear el Marshaller, y volcar la lista al fichero XML
+				Marshaller m = jaxbContext.createMarshaller();
+				m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+				m.marshal(jaxbElement, new FileOutputStream("./ventasarticulos.xml"));
+				System.out.println("Venta " + numeventa + " modificado.");
+				modifOk = true;
+
+			} else {
+				System.out.println("El número de venta " + numeventa + " no existe");
+				modifOk = false;
+			}
+
+		} catch (JAXBException je) {
+			System.out.println(je.getCause());
+		} catch (IOException ioe) {
+			System.out.println(ioe.getMessage());
+		}
+
+		return modifOk;
+
 	}
 
 	public static void visualizarxml() {
@@ -149,18 +353,18 @@ public class principal {
 			Ventas vent = miventa.getVentas();
 
 			// Guardar las ventas en la lista
-			List <Ventas.Venta>listaVentas = new ArrayList<>();
+			List<Ventas.Venta> listaVentas = new ArrayList<>();
 			listaVentas = vent.getVenta();
 
 			// Comprobar si existe el número de venta recorriendo el ArrayList
-		
-			boolean existe = verificarVenta(numeventa, listaVentas);;
-			
+
+			Ventas.Venta ve = verificarVenta(numeventa, listaVentas);
 
 			// Si el número de venta no existe, se añade la venta
-			if (!existe) {
+			if (ve == null) {
 				// Crear el objeto Ventas.Venta, y añadirle los datos
-				Ventas.Venta ve = new Ventas.Venta();
+
+				ve = new Ventas.Venta();
 				ve.setNombrecliente(nomcli);
 				ve.setFecha(fecha);
 				ve.setUnidades(uni);
@@ -185,13 +389,15 @@ public class principal {
 
 	}
 
-	private static boolean verificarVenta(int numeventa, List<Ventas.Venta> listaVentas) {
-		for (Ventas.Venta venta: listaVentas) {
+	private static Ventas.Venta verificarVenta(int numeventa, List<Ventas.Venta> listaVentas) {
+		for (Ventas.Venta venta : listaVentas) {
 			if (numeventa == venta.getNumventa().intValue()) {
-				return true;
+
+				return venta;
+
 			}
 		}
-		return false;
+		return null;
 	}
 
 }
